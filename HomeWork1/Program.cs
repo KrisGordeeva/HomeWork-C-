020814a771e80edaf7b45983f8d80a582a5d2ab4
Kris <data.base@rambler.ru>
2022-10-18 14:29:44 +0300

    HomeWork1

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,77 @@
+﻿//Задача  2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+/*Console.Write("Введите первое число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+bool r0 = n1 > n2;
+bool r1 = n1 == n2;
+bool r2 = n1 < n2;
+
+if (r0)
+{
+    Console.WriteLine($"Число {n1} больше чем число {n2}");
+}
+if (r1)
+{
+    Console.WriteLine($"Число {n1} равно числу {n2}");
+}
+if (r2)
+{
+    Console.WriteLine($"Число {n1} меньше числа {n2}");
+}
+*/
+
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+/*Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+*/
+
+
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+/*Console.Write("Ведите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else 
+{
+    Console.WriteLine("Нет");
+}
+*/
+
+
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+/*Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int x = 1; x < n; x++)
+if (x % 2 == 0)
+Console.Write( x + " ");
+*/
+
+
+
